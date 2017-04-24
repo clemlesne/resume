67bef1a871453d41a8c06991ae2f59217832d3e7
Clément Lesné <clem0925@gmail.com>
2017-04-24 15:52:51 +0200

    - Ajout d'une seconde version - Correction de marges et effets JS

---
+++ app/js/10_header.js
@@ -0,0 +1,153 @@
+(function () {
+
+    var width, height, largeHeader, canvas, ctx, points, target,
+        animateHeader = true,
+        circleColor = "132,255,195",
+        lineColor = circleColor,
+        alphaGlobal = 1;
+
+    // Main
+    initHeader();
+    initAnimation();
+
+    function initHeader() {
+        largeHeader = document.getElementById('container--header');
+
+        width = largeHeader.clientWidth;
+        height = largeHeader.clientHeight;
+        target = {x: width / 6, y: height / 2};
+
+        canvas = document.getElementById('header__canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create points
+        points = [];
+        for (var x = 0; x < width; x = x + width / 12) {
+            for (var y = 0; y < height; y = y + height / 12) {
+                var px = x + Math.random() * width / 12;
+                var py = y + Math.random() * height / 12;
+                var p = {x: px, originX: px, y: py, originY: py};
+                points.push(p);
+            }
+        }
+
+        // for each point find the 5 closest points
+        for (var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for (var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if (!(p1 == p2)) {
+                    var placed = false;
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        // assign a circle to each point
+        for (var i in points) {
+            var c = new Circle(points[i], 2 + Math.random() * 2, circleColor);
+            points[i].circle = c;
+        }
+    }
+
+    // animation
+    function initAnimation() {
+        animate();
+        for (var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if (animateHeader) {
+            ctx.clearRect(0, 0, width, height);
+            for (var i in points) {
+                // detect points in range
+                if (Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1 + 1 * Math.random(), {
+            x: p.originX - 50 + Math.random() * 100,
+            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
+            onComplete: function () {
+                shiftPoint(p);
+            }
+        });
+    }
+
+    // Canvas manipulation
+    function drawLines(p) {
+        if (!p.active) return;
+        for (var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(' + lineColor + ',' + p.active * alphaGlobal + ')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos, rad, color) {
+        var _this = this;
+
+        // constructor
+        (function () {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function () {
+            if (!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(' + _this.color + ',' + _this.active  * alphaGlobal + ')';
+            ctx.fill();
+        };
+    }
+
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,12 @@
 const
     fs = require('fs'),
     gulp = require('gulp'),
+    pump = require('pump'),
     pug = require('gulp-pug'),
-    data = require('gulp-data'),
     sass = require('gulp-sass'),
     webp = require('gulp-webp'),
-    csso = require('gulp-csso'),
     uncss = require('gulp-uncss'),
-    gs = require('gulp-selectors'),
+    uglify = require('gulp-uglify'),
     pkg = require('./package.json'),
     concat = require('gulp-concat'),
     merge = require('merge-stream'),
@@ -19,30 +18,31 @@ const
     replace = require('gulp-replace'),
     imagemin = require('gulp-imagemin'),
     cleanCSS = require('gulp-clean-css'),
-    shorthand = require('gulp-shorthand'),
     replaceTask = require('gulp-replace-task'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifyInline = require('gulp-minify-inline'),
     browserSync = require('browser-sync').create();
 
 var paths = {
-    templates_compile_data: 'app/views/data.json',
-    templates_compile_test: 'app/views/**/*.pug',
-    templates_compile_test_pages: 'app/views/pages/*.pug',
-    templates_tmp: '.tmp',
-    templates_tmp_test: '.tmp/*.html',
-    templates_dest: 'dist',
-    images_compile_test: 'app/img/**/*',
-    images_dist: 'dist/img',
-    css_compile: 'app/css',
-    css_compile_test: 'app/css/**/*.css',
-    css_tmp: '.tmp/css',
-    css_tmp_test: '.tmp/css/*.css',
-    sass_compile: 'app/scss',
-    sass_compile_test: 'app/scss/**/*.scss',
-    sass_tmp: '.tmp/css',
-    dist: 'dist',
-    tmp_selectors: ['.tmp/css/app.css', '.tmp/*.html', '!.tmp/_.html']
+    templates__test: 'app/views/**/*.pug',
+    templates_pages__test: 'app/views/pages/*.pug',
+    templates_html__test: 'dist/**/*.html',
+    templates_html__dist: 'dist',
+    images__test: 'app/img/**/*',
+    images__dist: 'dist/img',
+    css__test: 'app/css/**/*.css',
+    css__dist: 'dist/css',
+    sass: 'app/scss',
+    sass__test: 'app/scss/**/*.scss',
+    sass__dist: '.tmp/css',
+    fonts__test: 'app/fonts/**/*',
+    fonts__dist: 'dist/fonts',
+    js__test: 'app/js/**/*.js',
+    js__dist: 'dist/js',
+    js__dist_filename: 'app.js',
+    other__test: 'app/*',
+    other__dist: 'dist',
+    all_test: 'dist/**/*',
+    all_dist: 'dist'
 };
 
 // Static server
@@ -50,35 +50,104 @@ gulp.task('serve', ['compile'], function() {
     browserSync
         .init({
             server: {
-                baseDir: paths.dist
+                baseDir: 'dist'
             }
         });
 
-    gulp.watch(paths.images_compile_test, ['images:copy']);
-    gulp.watch([paths.templates_compile_test, paths.sass_compile_test, paths.css_compile_test, paths.sass_compile_test], ['html:selectors']);
+    gulp.watch(paths.js__test, ['js']);
+    gulp.watch(paths.images__test, ['images']);
+    gulp.watch(paths.fonts__test, ['fonts__copy']);
+    gulp.watch(paths.other__test, ['other__copy']);
+    gulp.watch(paths.templates__test, ['templates__compile']);
+    gulp.watch([paths.css__test, paths.sass__test], ['css__compile']);
 });
 
-gulp.task('compile', ['html:selectors', 'images:copy']);
+gulp.task('compile', ['js', 'images', 'fonts__copy', 'other__copy', 'templates__compile', 'css__compile']);
 
-gulp.task('html:selectors', ['html:annotations'], function () {
-
-    gulp.src(paths.tmp_selectors)
-        .pipe(gs.run({
-            'css': ['css'],
-            'html': ['html']
-        }, {
-            classes: ['fa*'],
-            ids: '*'
-        }))
-        .pipe(gulp.dest(paths.templates_dest))
+gulp.task('js', function () {
+    return gulp.src(paths.js__test)
+    // .pipe(uglify())
+        .pipe(concat(paths.js__dist_filename))
+        .pipe(gulp.dest(paths.js__dist))
         .pipe(browserSync.stream())
         .on('error', function (error) {
             console.error('' + error);
         });
 });
 
-gulp.task('html:annotations', ['html:compile'], function () {
-    return gulp.src(paths.templates_tmp_test)
+gulp.task('images', ['images_default__optimize', 'images_webp__optimize']);
+
+gulp.task('images_webp__optimize', function () {
+    return gulp.src(paths.images__test)
+        .pipe(rename(function (path) {
+            path.extname += path.extname == '.png' || path.extname == '.jpg' ? path.extname : '';
+        }))
+        .pipe(webp({
+            'method': 6
+        }))
+        .pipe(gulp.dest(paths.images__dist))
+        .pipe(browserSync.stream())
+        .on('error', function (error) {
+            console.error('' + error);
+        });
+});
+
+gulp.task('images_default__optimize', function () {
+    return gulp.src(paths.images__test)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images__dist))
+        .pipe(browserSync.stream())
+        .on('error', function (error) {
+            console.error('' + error);
+        });
+});
+
+gulp.task('fonts__copy', function () {
+    return gulp.src(paths.fonts__test)
+        .pipe(gulp.dest(paths.fonts__dist))
+        .pipe(browserSync.stream())
+        .on('error', function (error) {
+            console.error('' + error);
+        });
+});
+
+gulp.task('css__compile', function () {
+    var sassStream,
+        cssStream;
+
+    sassStream = gulp.src(paths.sass__test)
+        .pipe(compass({
+            project: __dirname,
+            css: paths.sass__dist,
+            sass: paths.sass,
+            image: paths.images
+        }))
+        // .pipe(uncss({
+        //     html: [paths.templates_html__test],
+        //     ignore: [new RegExp('amp*'), new RegExp('wp__*'), '.hidden']
+        // }))
+        .pipe(cleanCSS({
+            compatibility: 'ie8',
+            advanced: true,
+            roundingPrecision: -1
+        }))
+        .on('error', function (error) {
+            console.error('' + error);
+        });
+
+    cssStream =
+        gulp.src(paths.css__test)
+            .on('error', function (error) {
+                console.error('' + error);
+            });
+
+    return merge(cssStream, sassStream)
+        .pipe(autoprefixer({
+            browsers: ['> 3%', 'IE 8'],
+            cascade: true
+        }))
+        .pipe(concat('app.css'))
+        .pipe(replace('@charset "UTF-8";', ''))
         .pipe(replaceTask({
             patterns: [
                 {
@@ -91,101 +160,47 @@ gulp.task('html:annotations', ['html:compile'], function () {
                 }
             ]
         }))
-        .pipe(minifyInline())
-        .pipe(htmlmin({
-            useShortDoctype: true,
-            sortClassName: true,
-            sortAttributes: true,
-            removeRedundantAttributes: true,
-            removeComments: true,
-            quoteCharacter: '"'
-        }))
-        .pipe(gulp.dest(paths.templates_tmp))
+        .pipe(gulp.dest(paths.css__dist))
         .pipe(browserSync.stream())
         .on('error', function (error) {
             console.error('' + error);
         });
 });
 
-gulp.task('html:compile', ['css:compile'], function () {
-    return gulp.src(paths.templates_compile_test_pages)
-        .pipe(data(function(file) {
-            return JSON.parse(
-                fs.readFileSync(paths.templates_compile_data)
-            );
-        }))
+gulp.task('templates__compile', function () {
+    return gulp.src(paths.templates_pages__test)
         .pipe(pug())
-        .pipe(gulp.dest(paths.templates_tmp))
-        .on('error', function (error) {
-            console.error('' + error);
-        });
-});
-
-gulp.task('css:compile', function () {
-    var sassStream,
-        cssStream;
-
-    sassStream = gulp.src(paths.sass_compile_test)
-        .pipe(compass({
-            project: __dirname,
-            css: paths.css_tmp,
-            sass: paths.sass_compile
+        .pipe(htmlmin({
+            minifyURLs: true,
+            sortAttributes: true,
+            sortClassName: true,
+            useShortDoctype: true,
+            removeAttributeQuotes: true,
+            quoteCharacter: '\"',
+            minifyJS: true
         }))
-        .pipe(uncss({
-            html: [paths.templates_tmp_test]
+        .pipe(replaceTask({
+            patterns: [
+                {
+                    match: 'timestamp',
+                    replacement: new Date().getTime()
+                },
+                {
+                    match: 'version',
+                    replacement: pkg.version
+                }
+            ]
         }))
-        .pipe(shorthand())
-        .on('error', function (error) {
-            console.error('' + error);
-        });
-
-    cssStream =
-        gulp.src(paths.css_compile_test)
-            .on('error', function (error) {
-                console.error('' + error);
-            });
-
-    return merge(cssStream, sassStream)
-        .pipe(autoprefixer({
-            browsers: ['> 3%', 'IE 8'],
-            cascade: true
-        }))
-        .pipe(cleanCSS({
-            compatibility: 'ie8',
-            advanced: true,
-            roundingPrecision: -1
-        }))
-        .pipe(concat('app.css'))
-        .pipe(replace('@charset "UTF-8";', ''))
-        .pipe(csso())
-        .pipe(gulp.dest(paths.css_tmp))
+        .pipe(gulp.dest(paths.templates_html__dist))
         .pipe(browserSync.stream())
         .on('error', function (error) {
             console.error('' + error);
         });
 });
 
-gulp.task('images:copy', ['images:compile:standard', 'images:compile:webp']);
-
-gulp.task('images:compile:standard', function () {
-    return gulp.src(paths.images_compile_test)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.images_dist))
-        .pipe(browserSync.stream())
-        .on('error', function (error) {
-            console.error('' + error);
-        });
-});
-
-gulp.task('images:compile:webp', function () {
-    return gulp.src(paths.images_compile_test)
-        .pipe(rename(function (path) {
-            path.extname += path.extname == '.png' || path.extname == '.jpg' ? path.extname : '';
-        }))
-        .pipe(webp({
-            'method': 6
-        }))
-        .pipe(gulp.dest(paths.images_dist))
+gulp.task('other__copy', function () {
+    gulp.src(paths.other__test)
+        .pipe(gulp.dest(paths.other__dist))
         .pipe(browserSync.stream())
         .on('error', function (error) {
             console.error('' + error);
