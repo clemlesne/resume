a8599d2da24d7c11f61b861a2769377a59fff2e4
Clément Lesné <clementlesne.cl@gmail.com>
2019-04-09 00:50:27 +0200

    Switch to Gulp v4 and add job@AXA

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-'use strict';
-
 const
     fs = require('fs'),
     gulp = require('gulp'),
     http = require('http'),
     glob = require('glob'),
     pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
     gutil = require('gulp-util'),
     uncss = require('gulp-uncss'),
     babel = require('gulp-babel'),
@@ -16,7 +15,6 @@ const
     cleanCSS = require('clean-css'),
     penthouse = require('penthouse'),
     htmlmin = require('gulp-htmlmin'),
-    compass = require('gulp-compass'),
     replace = require('gulp-replace'),
     responsive = require('gulp-responsive'),
     gulpCleanCSS = require('gulp-clean-css'),
@@ -53,30 +51,179 @@ var paths = {
     all_dist: 'dist'
 };
 
-var getPackageJson = function () {
+var getPackageJson = () => {
     return JSON.parse(fs.readFileSync('package.json', 'utf8'));
 };
 
-// Static server
-gulp.task('serve', ['compile'], function() {
-    browserSync
-        .init({
-            server: {
-                baseDir: 'dist'
-            }
-        });
-
-    gulp.watch([paths.js_sw__test, paths.js_app__test], ['js']);
-    gulp.watch(paths.images__test, ['images']);
-    gulp.watch(paths.fonts__test, ['fonts__copy']);
-    gulp.watch([paths.other_root__test], ['other']);
-    gulp.watch(paths.templates__test, ['templates__compile']);
-    gulp.watch([paths.css__test, paths.sass__test], ['css__compile']);
+gulp.task('js__app', () => {
+    return gulp.src(paths.js_app__test)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify({
+            warnings: false,
+            parse: {
+                html5_comments: false
+            },
+            compress: {
+                sequences: 200,
+                unused: true,
+                properties: true,
+                drop_debugger: true,
+                conditionals: true,
+                comparisons: true,
+                evaluate: true,
+                booleans: true,
+                loops: true,
+                toplevel: true,
+                hoist_funs: true,
+                hoist_vars: true,
+                if_return: true,
+                join_vars: true,
+                collapse_vars: true,
+                reduce_vars: true,
+                negate_iife: true,
+                passes: 10
+            },
+            output:  {
+                beautify: false,
+                webkit: true,
+                wrap_iife: true,
+            },
+            ie8: false
+        }))
+        .pipe(concat(paths.js_app__dist_filename))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.js_app__dist))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('compile', ['js', 'images', 'fonts__copy', 'other', 'templates__compile', 'css__compile']);
+gulp.task('js', gulp.parallel('js__app'));
 
-gulp.task('compile-optimized', ['compile'], function() {
+gulp.task('images__raw', () => {
+    return gulp.src(paths.images_raw__test)
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.images_raw__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images__opt', () => {
+    return gulp.src(paths.images_opt__test)
+        .pipe(responsive(
+            {
+                '**/*': [{
+                    width: 1024
+                }]
+            }, {
+                quality: 80,
+                compressionLevel: 9,
+                progressive: true,
+                withMetadata: false,
+                errorOnEnlargement: false
+            }))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.images_opt__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', gulp.parallel('images__raw', 'images__opt'));
+
+gulp.task('fonts__copy', () => {
+    return gulp.src(paths.fonts__test)
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.fonts__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('css__compile', () => {
+    var sassStream,
+        cssStream;
+
+    sassStream = gulp.src(paths.sass__test)
+        .pipe(sass())
+        // .pipe(uncss({
+        //     html: [paths.templates_html__test],
+        //     ignore: [new RegExp('amp*'), new RegExp('wp__*'), '.hidden']
+        // }))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); });
+
+    cssStream = gulp.src(paths.css__test)
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); });
+
+    return merge(cssStream, sassStream)
+        .pipe(autoprefixer({
+            browsers: ['> 3%'],
+            cascade: true
+        }))
+        .pipe(concat('app.css'))
+        .pipe(replace('@charset "UTF-8";', ''))
+        .pipe(replaceTask({
+            patterns: [
+                {
+                    match: 'version',
+                    replacement: getPackageJson().version
+                }
+            ]
+        }))
+        .pipe(gulpCleanCSS({
+            compatibility: 'ie9',
+            advanced: true,
+            roundingPrecision: 3
+        }))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.css__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('templates__compile', () => {
+    return gulp.src(paths.templates_pages__test)
+        .pipe(pug())
+        .pipe(htmlmin({
+            minifyURLs: true,
+            sortAttributes: true,
+            sortClassName: true,
+            useShortDoctype: true,
+            removeAttributeQuotes: true,
+            quoteCharacter: '\"',
+            minifyJS: true
+        }))
+        .pipe(replaceTask({
+            patterns: [
+                {
+                    match: 'version',
+                    replacement: getPackageJson().version
+                }
+            ]
+        }))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.templates_html__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('other_root__copy', () => {
+    gulp.src(paths.other_root__test)
+        .pipe(replaceTask({
+            patterns: [
+                {
+                    match: 'version',
+                    replacement: getPackageJson().version
+                },
+                {
+                    match: 'timestamp',
+                    replacement: new Date().getTime()
+                }
+            ]
+        }))
+        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
+        .pipe(gulp.dest(paths.other_root__dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('other', gulp.parallel('other_root__copy'));
+
+gulp.task('compile', gulp.parallel('js', 'images', 'fonts__copy', 'other', 'templates__compile', 'css__compile'));
+
+gulp.task('compile-optimized', gulp.parallel('compile', () => {
     var file = new staticServer.Server('dist', {
         cache: 3600
     });
@@ -194,175 +341,21 @@ gulp.task('compile-optimized', ['compile'], function() {
             doCriticalize($arrTmp.splice(0, $arrSize), 0, callbackEnd);
         }
     });
-});
+}));
 
-gulp.task('js', ['js__app']);
+// Static server
+gulp.task('serve', gulp.parallel('compile', () => {
+    browserSync
+        .init({
+            server: {
+                baseDir: 'dist'
+            }
+        });
 
-gulp.task('js__app', function () {
-    return gulp.src(paths.js_app__test)
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify({
-            warnings: false,
-            parse: {
-                html5_comments: false
-            },
-            compress: {
-                sequences: 200,
-                unused: true,
-                properties: true,
-                drop_debugger: true,
-                conditionals: true,
-                comparisons: true,
-                evaluate: true,
-                booleans: true,
-                loops: true,
-                toplevel: true,
-                hoist_funs: true,
-                hoist_vars: true,
-                if_return: true,
-                join_vars: true,
-                collapse_vars: true,
-                reduce_vars: true,
-                negate_iife: true,
-                passes: 10
-            },
-            output:  {
-                beautify: false,
-                webkit: true,
-                wrap_iife: true,
-            },
-            ie8: false
-        }))
-        .pipe(concat(paths.js_app__dist_filename))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.js_app__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('images', ['images__raw', 'images__opt']);
-
-gulp.task('images__raw', function () {
-    return gulp.src(paths.images_raw__test)
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.images_raw__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('images__opt', function () {
-    return gulp.src(paths.images_opt__test)
-        .pipe(responsive(
-            {
-                '**/*': [{
-                    width: 1024
-                }]
-            }, {
-                quality: 80,
-                compressionLevel: 9,
-                progressive: true,
-                withMetadata: false,
-                errorOnEnlargement: false
-            }))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.images_opt__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('fonts__copy', function () {
-    return gulp.src(paths.fonts__test)
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.fonts__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('css__compile', function () {
-    var sassStream,
-        cssStream;
-
-    sassStream = gulp.src(paths.sass__test)
-        .pipe(compass({
-            project: __dirname,
-            css: paths.sass__dist,
-            sass: paths.sass,
-            image: paths.images
-        }))
-        // .pipe(uncss({
-        //     html: [paths.templates_html__test],
-        //     ignore: [new RegExp('amp*'), new RegExp('wp__*'), '.hidden']
-        // }))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); });
-
-    cssStream = gulp.src(paths.css__test)
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); });
-
-    return merge(cssStream, sassStream)
-        .pipe(autoprefixer({
-            browsers: ['> 3%'],
-            cascade: true
-        }))
-        .pipe(concat('app.css'))
-        .pipe(replace('@charset "UTF-8";', ''))
-        .pipe(replaceTask({
-            patterns: [
-                {
-                    match: 'version',
-                    replacement: getPackageJson().version
-                }
-            ]
-        }))
-        .pipe(gulpCleanCSS({
-            compatibility: 'ie9',
-            advanced: true,
-            roundingPrecision: 3
-        }))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.css__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('templates__compile', function () {
-    return gulp.src(paths.templates_pages__test)
-        .pipe(pug())
-        .pipe(htmlmin({
-            minifyURLs: true,
-            sortAttributes: true,
-            sortClassName: true,
-            useShortDoctype: true,
-            removeAttributeQuotes: true,
-            quoteCharacter: '\"',
-            minifyJS: true
-        }))
-        .pipe(replaceTask({
-            patterns: [
-                {
-                    match: 'version',
-                    replacement: getPackageJson().version
-                }
-            ]
-        }))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.templates_html__dist))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('other', ['other_root__copy']);
-
-gulp.task('other_root__copy', function () {
-    gulp.src(paths.other_root__test)
-        .pipe(replaceTask({
-            patterns: [
-                {
-                    match: 'version',
-                    replacement: getPackageJson().version
-                },
-                {
-                    match: 'timestamp',
-                    replacement: new Date().getTime()
-                }
-            ]
-        }))
-        .on('error', function($err) { gutil.log(gutil.colors.red($err.message)); })
-        .pipe(gulp.dest(paths.other_root__dist))
-        .pipe(browserSync.stream());
-});
+    gulp.watch([paths.js_sw__test, paths.js_app__test], gulp.parallel('js'));
+    gulp.watch(paths.images__test, gulp.parallel('images'));
+    gulp.watch(paths.fonts__test, gulp.parallel('fonts__copy'));
+    gulp.watch([paths.other_root__test], gulp.parallel('other'));
+    gulp.watch(paths.templates__test, gulp.parallel('templates__compile'));
+    gulp.watch([paths.css__test, paths.sass__test], gulp.parallel('css__compile'));
+}));
